a3d55080c681f19cea0e44e87fc3f123f86f5af4
Rogier Knoester <knoesterrogier@gmail.com>
2019-05-18 14:03:23 +0200

    update setup

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 import setuptools
 
 with open("README.md", "r") as fh:
-
     long_description = fh.read()
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setuptools.setup(name='omniart_eye_generator',
-                 version='0.1',
+                 version='0.1.1',
                  description='A cDCGAN that can generate eyes',
                  long_description=long_description,
                  long_description_content_type="text/markdown",
@@ -15,4 +17,5 @@ setuptools.setup(name='omniart_eye_generator',
                  license='MIT',
                  packages=setuptools.find_packages(),
                  package_data={'omniart_eye_generator': ['*.pth']},
+                 install_requires=requirements,
                  zip_safe=False)
